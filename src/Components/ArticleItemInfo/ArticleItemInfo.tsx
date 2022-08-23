3598cd38d095fc1432e3a98fa71b7830f3da9c31
Vladkrivonossov <vladik374@gmail.com>
2022-08-23 12:50:38 +0300

    Browser router + article item info with link and author

---
+++ src/Components/ArticleItemInfo/ArticleItemInfo.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+import './ArticleItemInfo.css';
+
+interface Props {
+  categoryName: string;
+  date: string;
+  sourceLink?: string;
+  sourceName?: string;
+  author?: string;
+}
+export const ArticleItemInfo: FC<Props> = ({ categoryName, date, sourceName, sourceLink, author }) => {
+  return (
+    <div className="grid">
+      <div className="article-item-info__category-container">
+        <span className="article-category article-item-info__category">{categoryName}</span>
+        {sourceLink && (
+          <a className="article-item-info__link" href={sourceLink} target="_blank" rel="noreferrer">
+            Источник:{sourceName}
+            {author && <span className="article-item-info__author">{author}</span>}
+          </a>
+        )}
+      </div>
+      <span className="article-date article-item-info__date">{date}</span>
+    </div>
+  );
+};
--- src/script.tsx
+++ src/script.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import './common.css';
 import { App } from './Components/App/App';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <Router>
